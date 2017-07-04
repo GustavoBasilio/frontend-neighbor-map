38fc0b9f90f172a706575969d727fca72d0b0d97
Gustavo Basilio <gusbpb@gmail.com>
2017-07-04 20:35:57 -0300

    Added foursquare request

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -9,15 +9,18 @@ function Location(lat, lng) {
 }
 
 //Create the html for the infobox
-function createInfobox(title,subtitle) {
+function createInfobox(title,subtitle,image) {
     var dom = document.createElement('div'),
         titleDom = document.createElement('h2'),
-        subtitleDom = document.createElement('h3');
+        subtitleDom = document.createElement('h3'),
+        imageDom = document.createElement('img');
+    imageDom.src = image;
     titleDom.innerHTML = title;
     subtitleDom.innerHTML = subtitle;
     dom.classList = "info-window";
     dom.appendChild(titleDom);
     dom.appendChild(subtitleDom);
+    dom.appendChild(imageDom);
     return dom.outerHTML;
 }
 
@@ -133,6 +136,7 @@ function MapViewModel() {
         },function(response,status) {
             self.markers = [];
             response.map(function(place) {
+                var position = place.geometry.location.lat()+','+place.geometry.location.lng();
                 self.markers.push({
                     marker: new google.maps.Marker({
                         position: new Location(place.geometry.location.lat(),place.geometry.location.lng()).position,
@@ -141,6 +145,7 @@ function MapViewModel() {
                         animation: google.maps.Animation.DROP,
                         icon: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
                     }),
+                    position: position,
                     name: place.name,
                     address: place.vicinity,
                     status: 1
@@ -157,10 +162,41 @@ function MapViewModel() {
     self.getLocations(self.center);
     //Open infowindow
     self.onpenInfoWIndow = function(marker) {
-        marker.marker.setAnimation(google.maps.Animation.BOUNCE);
-        setTimeout(function(){ marker.marker.setAnimation(null); }, 750);
-        infoWindow.setContent(createInfobox(marker.name, marker.address));
-        infoWindow.open(map, marker.marker);
+        $.ajax({
+            url: "https://api.foursquare.com/v2/venues/search",
+            method: "get",
+            dataType: "jsonp",
+            data: {
+                "client_id": "1JXUG0BCBAIWUM0WDDAVQT3CY0PVH4FSXRC0LJL5OVKOIZZ2",
+                "client_secret": "5B4NN4OWKVET0Y0EJWEWKLWRPZISLSQKJZXXHCYOKQNTP442",
+                "limit": 1,
+                "query": marker.name,
+                "ll": marker.position,
+                "v": '20170704',
+                "m":'foursquare'
+            }
+        }).done(function(data) {
+            $.ajax({
+                url: "https://api.foursquare.com/v2/venues/"+data.response.venues[0].id+"/photos",
+                method: "get",
+                dataType: "jsonp",
+                data: {
+                    "client_id": "1JXUG0BCBAIWUM0WDDAVQT3CY0PVH4FSXRC0LJL5OVKOIZZ2",
+                    "client_secret": "5B4NN4OWKVET0Y0EJWEWKLWRPZISLSQKJZXXHCYOKQNTP442",
+                    "VENUE_ID": data.response.venues[0].id,
+                    "limit": 1,
+                    "v": '20170704',
+                    "m":'foursquare'
+                }
+            }).done(function(photos) {
+                var photo = photos.response.photos.items[0];
+                var image = photo.prefix+'200x80'+photo.suffix;
+                marker.marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function(){ marker.marker.setAnimation(null); }, 750);
+                infoWindow.setContent(createInfobox(marker.name, marker.address, image));
+                infoWindow.open(map, marker.marker);
+            });
+        });
     };
 }
 
